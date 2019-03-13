36d39d20ddb8fbd2054c4b620947ccd5deebc23a
github username <github email address>
2019-03-12 23:07:11 -0400

    progress- chea

--- app/controllers/eatery_controller.rb
+++ app/controllers/eatery_controller.rb
@@ -3,7 +3,7 @@ class EateryController < AppController
   #Read
   get '/eateries' do 
     if  Helpers.logged_in?(session)
-      @eateries = Eatery.all
+      @eateries = Eatery.all 
       erb :'eatery/eateries'
     else 
       redirect '/login'
@@ -15,20 +15,21 @@ class EateryController < AppController
     if Helpers.logged_in?(session) 
       erb :'/eatery/new'
     else 
-      flash[:message] = "Oops! It looks like you have not logged in yet."
+      # flash[:message] = "Oops! It looks like you have not logged in yet."
       redirect '/login'
     end 
-  end 
+  end   
   
-  # Create
+
   post '/eateries' do 
     user= Helpers.current_user(session)
     if params[:name] == "" || params[:cuisine] == "" || params[:rating] == "" || params[:content] == "" || params[:number] == ""
-      flash[:message] = "To give a proper review, please fill out the necessary information."
+      flash[:message] = "To give a proper review, please fill out all sections."
       redirect to '/eatery/new'
-    end 
-      @eateries = Eatery.create(name: params[:name], cuisine: params[:cuisine], rating: params[:rating], content: params[:content], number: params[:number], user_id: user.id)
+    else 
+      eatery = Eatery.create(name: params[:name], cuisine: params[:cuisine], rating: params[:rating], content: params[:content], number: params[:number], user_id: user.id)
       redirect '/eateries'
+      end 
     end 
   end 
 
@@ -36,39 +37,40 @@ class EateryController < AppController
     if !Helpers.is_logged_in?(session)
       redirect to '/login'
       flash[:message] = "Looks like you weren't logged in yet. Please log in below."
-    elsif
-      @eateries.user_id == session[:user_id]
-      erb :'/show_eatery'
-    else 
-      @eateries.user_id != session[:user_id]
-      erb :"eatery/eateries"
     end 
+      @eatery = Eatery.find_by(session[:user_id])
+      erb :'/show_eatery'
+    # else 
+    #   @eateries.user_id != session[:user_id]
+    #   erb :"eatery/eateries"
   end
 
   #Edit/Update
   get '/eatery/:id/edit' do
-    if !Helpers.is_logged_in?(session)
+    if !Helpers.logged_in?(session)
       flash[:message] = "Looks like you weren't logged in yet. Please log in below."
-      redirect to '/login'
+      redirect '/login'
     end
-    @eateries = Eatery.find(params[:id])
-    if Helpers.current_user(session).id != @eateries.user_id
-      flash[:wrong_user_edit] = "Sorry you can only edit restaurants that you yourself have reviewed"
-      redirect to '/eateries'
-    end
-    erb :"/edit_eatery"
+    @eatery = Eatery.find(params[:id])
+    if Helpers.current_user(session).id == session[:user_id]
+      erb :"/edit_eatery"
+    else 
+    flash[:wrong_user_edit] = "Sorry you can only edit restaurants that you yourself have reviewed"
+      redirect '/eateries'
+    end 
   end
 
-  # post '/eateries' do 
-  #   user = Helpers.current_user(session)
-  #   if !params.has_value("")
-  #     redirect to '/eateries/new'
-  #   else
-  #     eateries = Eatery.create(name: params[:name], cuisine: params[:cuisine], rating: params[:rating], content: params[:content], number: params[:number], user_id: user.id)
-  #     redirect to '/eateries'
-  #   end 
-  # end
-# end 
+ patch '/eateries/:id' do 
+  if params[:name] == "" || params[:cuisine] == "" || params[:rating] == "" || params[:content] == ""
+      flash[:message] = "Try again, please update all category fields."
+      redirect to "/reviews/#{params[:id]}/edit"
+  eatery = Eatery.find_by(params[:id])
+
+  eatery.update(params[:name], params[:cuisine], params[:rating], params[:content], params[:number])
+  eatery.save 
+  # flash[:messsage] = "Your review has been updated!"
+  redirect "/eateries/#{@eatery.id}"
+ end 
 
 
 
